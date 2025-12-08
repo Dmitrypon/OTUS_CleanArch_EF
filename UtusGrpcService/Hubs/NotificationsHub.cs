8d475f49e5a49da778f63e856892abcc2da474f5
Dmitry Ponomar <mitrybox@gmail.com>
2025-12-08 14:36:34 +0300

    NotificationsHub added

---
+++ UtusGrpcService/Hubs/NotificationsHub.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace UtusGrpcService.Hubs
+{
+    public class NotificationsHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
+
+
